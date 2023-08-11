18e4a2a3200a7ef22ff64fb974305ea80d90cdd2
Keniluv2 <ayenideborah94@gmail.com>
2023-08-11 00:19:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <Stdio.h>
+/**
+ * main - Program that print a line using the put function
+ * Return: o (when it returns o is a Success)
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
