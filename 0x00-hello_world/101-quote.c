1353f63cbc27121323e73572d46ea2fdddb57fd7
Keniluv2 <ayenideborah94@gmail.com>
2023-08-12 02:54:23 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Enty point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	wite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	return (1)
+}
