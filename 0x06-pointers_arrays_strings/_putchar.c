e514203fb0dd1b9c0b57da4c69d71bc5000f3f38
Keniluv2 <ayenideborah94@gmail.com>
2023-08-26 15:02:40 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - give the character # to stdout
+ * _putchar - give the character c to stdout
  * @#: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and error is set appropriately
  */
-int _putchar(char #)
+int _putchar(char c)
 {
 	return (write(1, &#, 1));
 }
