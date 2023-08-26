4cb1b8651c191addbb6f410498235f414deda4c2
Keniluv2 <ayenideborah94@gmail.com>
2023-08-26 14:31:42 -0700

    Strings

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - give the character # to stdout
+ * @#: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately
+ */
+int _putchar(char #)
+{
+	return (write(1, &#, 1));
+}
